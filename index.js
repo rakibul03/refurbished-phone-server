cf32be33a78259c6721fc1d2594a49eb3f119b8e
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2022-11-27 16:52:54 +0530

    Create two endpoint for seller products info hanlde

--- index.js
+++ index.js
@@ -39,7 +39,10 @@ async function run() {
     // Get specific prodcuts for specific id
     app.get("/category/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { category_id: id };
+      const query = {
+        category_id: id,
+        isSold: false,
+      };
       const result = await productsCollections.find(query).toArray();
       res.send(result);
     });
@@ -49,6 +52,14 @@ async function run() {
       const email = req.query.email;
       const query = { seller_email: email };
       const result = await productsCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // Endpoint for delete a products from my products in dashboard
+    app.delete("/dashboard/my-products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollections.deleteOne(query);
       res.send(result);
     });
 
@@ -63,6 +74,14 @@ async function run() {
     app.get("/users/seller", async (req, res) => {
       const query = { role: "seller" };
       const result = await usersCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    //get seller for verification
+    app.get("/users/verify", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email, role: "seller" };
+      const result = await usersCollections.findOne(query);
       res.send(result);
     });
 
@@ -89,22 +108,74 @@ async function run() {
       res.send(result);
     });
 
+    // check user verify or not
+    app.patch("/users/seller/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          isVerifyed: status.status,
+        },
+      };
+      const result = await usersCollections.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    // add a products
     app.post("/add-products", async (req, res) => {
       const product = req.body;
       const result = await productsCollections.insertOne(product);
       res.send(result);
     });
 
+    // create booked products API endpoint
     app.post("/booking-products", async (req, res) => {
       const booking = req.body;
       const result = await bookingCollections.insertOne(booking);
       res.send(result);
     });
 
+    // booked a products for sell
     app.get("/booking-products", async (req, res) => {
       const email = req.query.email;
       const query = { userEmail: email };
       const result = await bookingCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    // api endpoint for ad post
+    app.get("/ad", async (req, res) => {
+      const query = { showAd: true, isAvailabe: true };
+      const result = await productsCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    //  api endpoint for ad update
+    app.patch("/ad/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          showAd: status.status,
+        },
+      };
+      const result = await productsCollections.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/stock/:id", async (req, res) => {
+      const id = req.params.id;
+      const status = req.body;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          isAvailabe: status.status,
+          showAd: status.ad,
+        },
+      };
+      const result = await productsCollections.updateOne(query, updateDoc);
       res.send(result);
     });
   } finally {
